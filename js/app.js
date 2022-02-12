d638180a43b0b923107dfb83e8e0679d3a8fde74
Jon Gitter <jonathan.m.gitter@gmail.com>
2022-02-11 23:05:38 -0700

    completed problem 3

--- js/app.js
+++ js/app.js
@@ -27,7 +27,7 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function multiply(a, b) { //eslint-disable-line
-  return [a * b, `The product of ${a} and ${b} is ${a * b}.`];
+  return [a * b, `The product of ${a} and ${b} is ${a * b}.`, 4 * 7 * a];
 }
 
 // Here is the test for multiply(); uncomment it to run it
@@ -47,12 +47,14 @@ IMPORTANT DETAIL: You may not use the arithmetic operators + and * in this funct
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSumAndMultiply() function and see if the test passes.*/
 
 // Write your code here
-function sumAndMultiply(a, b, c) { //eslint-disable-line
+function sumAndMultiply(a, b, c) { //eslint-disable-line 
+  return [sum(sum(a, b)[0], c)[0], multiply(multiply(a, b)[0], c)[0], `${a} and ${b} and ${c} sum to ${sum(sum(a, b)[0], c)[0]}.`, `The product of ${a} and ${b} and ${c} is ${multiply(multiply(a, b)[0], c)[0]}.`];
+  // return [16, 140, `${a} and ${b} and ${c} sum to 16.`, `The product of ${a} and ${b} and ${c} is 140.`];
 
 }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+testSumAndMultiply(4, 7, 5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
